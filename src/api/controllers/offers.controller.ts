96053f28439686399f4c3dfde809e548ce9632ac
Julio Truzzi <julio.truzzi@gmail.com>
2022-01-28 12:26:52 -0300

    Add notification on offer update/reject/revoke

--- src/api/controllers/offers.controller.ts
+++ src/api/controllers/offers.controller.ts
@@ -11,7 +11,10 @@ import OfferService from "../services/offer.service";
 import NFTService from "../services/nfts.service";
 import NotificationService from "../services/notification.service";
 import { NOTIFICATION_TYPES } from "../../db/notifications.model";
+import UserService from "../services/user.service";
 
+
+const userService = new UserService();
 const offerService = new OfferService();
 const nftService = new NFTService();
 const notificationService = new NotificationService();
@@ -81,11 +84,29 @@ export const updateOffer = async function (
       throw "Offer does not belong to user";
     }
 
+    const nft = await nftService.getDetails(offer.nft_id);
+
     await offerService.updateOffer(
       offerId,
       updateOfferRequestDTO.days_to_expire,
       updateOfferRequestDTO.amount
-    );
+    ).then(async (offer) => {
+      await notificationService
+        .createNotification({
+          type: NOTIFICATION_TYPES.OfferUpdated,
+          sender_user_id: session.near_api.user_info.user_id,
+          recipient_user_id: nft.data.owner_id,
+          data: {
+            sender_wallet_name: session.near_api.user_info.wallet_id,
+            amount: offer.amount,
+            nft_title: nft.data.title,
+            offer_id: offer.id,
+          },
+        })
+        .catch((err) => {
+          console.log("Could not create notification", err);
+        });
+    });;
 
     res.json({ message: "Offer updated" });
   } catch (error) {
@@ -159,7 +180,22 @@ export const rejectOffer = async function (
       throw "Offer does not belong to user";
     }
 
-    await offerService.rejectOffer(offerId);
+    await offerService.rejectOffer(offerId).then(async (offer) => {
+      const ownerDetails = await userService.getUserById(offer.owner_id);
+      await notificationService
+        .createNotification({
+          type: NOTIFICATION_TYPES.OfferRejected,
+          sender_user_id: offer.owner_id,
+          recipient_user_id: offer.user_id,
+          data: {
+            sender_wallet_name: ownerDetails.wallet_id,
+            offer_id: offer.id,
+          },
+        })
+        .catch((err) => {
+          console.log("Could not create notification", err);
+        });
+    });
     res.json({ message: "Offer rejected" });
   } catch (error) {
     return res.status(500).json({ error });
@@ -175,7 +211,22 @@ export const revokeOffer = async function (
   const session: TokenPayload = req.session;
   const offerId = req.params.id;
   try {
-    const offer = await offerService.getOfferById(offerId);
+    const offer = await offerService.getOfferById(offerId).then(async (offer) => {
+      const ownerDetails = await userService.getUserById(offer.user_id);
+      await notificationService
+        .createNotification({
+          type: NOTIFICATION_TYPES.OfferRevoked,
+          sender_user_id: offer.user_id,
+          recipient_user_id: offer.owner_id,
+          data: {
+            sender_wallet_name: ownerDetails.wallet_id,
+            offer_id: offer.id,
+          },
+        })
+        .catch((err) => {
+          console.log("Could not create notification", err);
+        });
+    });;
 
     if (offer.user_id !== session.near_api.user_info.user_id) {
       throw "Offer does not belong to user";
